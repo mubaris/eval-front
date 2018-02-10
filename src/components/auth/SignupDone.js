1c78c5f203b1a505b744130e9cc99c39d57ee584
Mubaris NK <mubarishassannk@gmail.com>
2018-02-10 19:16:54 +0530

    Disable Email Verification

--- src/components/auth/SignupDone.js
+++ src/components/auth/SignupDone.js
@@ -4,8 +4,7 @@ export default class SignupDone extends Component {
     render() {
         return (
             <h3 className="mx-5">
-                Thanks for your registration, please follow the link sent to your provided email to activate
-                your account.
+                Thanks for your registration, please login now!
             </h3>
         )
     }
